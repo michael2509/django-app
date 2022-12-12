ba180b0faa446f3f50cc9aaabccbf5f73bd196f8
Michael De Madet <michaeldemadet@gmail.com>
2022-12-12 16:43:02 +0100

    add authentication

--- library-project/settings.py
+++ library-project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'library-project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('customer/', views.customer, name='customer'),
+    path('bookseller/', views.bookseller, name='bookseller')
 ]
